551da4d57d6b487ae37cc1f0f7188cb30cb13553
Константин <cool.olo124@list.ru>
2023-07-30 16:41:54 +0400

    v1.0

---
+++ script.js
@@ -0,0 +1,5 @@
+document.body.setAttribute('scheme', 'vk_custom')
+document.body.className = ''
+// document.body.innerHTML += '<div class="vk_custom-params"><div class="vk_custom-main">1</div><div class="vk_custom-secondary">2</div></div>'
+
+// document.querySelector('.vk_custom-main')
